486847c3d4812db9bef8e1cdfd09135c8a2f1ca2
carjimdra <carjimdra@gmail.com>
2011-07-29 15:10:58 +0000

    added language file

---
+++ i18n/en_US/fileupload.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  File Upload English Language file
+ *
+ * This plugin was written for Ushahidi Liberia, by the contractors of Ushahidi Liberia
+ * 2011
+ *
+ * @package  File Upload plugin
+ * @author     Carter Draper <carjimdra@gmail.com>
+ * 
+ */
+
+$lang = array(
+'uploadfiles' => 'Upload Files',
+'uploadfiles_description' => 'Use this to upload files, such as PDFs, Word documents, and other files',
+'description' => 'Description:',
+'file' => 'File:',
+'add' => 'add',
+'incident_files' => 'Files',
+'uploaded' => 'Uploaded files associated with this report',
+'uploaded_files' => 'Uploaded Files',
+'page' =>'Uploaded files associated with this page:',
+'refresh' =>'Refresh files for this page',
+
+
+
+);
